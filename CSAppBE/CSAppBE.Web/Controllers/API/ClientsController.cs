0a34082e0c2b84a630c5208d26da65881479c19a
alenewberry <alenewberry@gmail.com>
2020-06-16 16:20:13 -0300

    Adding API Service

---
+++ CSAppBE/CSAppBE.Web/Controllers/API/ClientsController.cs
@@ -0,0 +1,22 @@
+﻿namespace CSAppBE.Web.Controllers.API
+{
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[Controller]")]
+    public class ClientsController : Controller
+    {
+        private readonly IClientRepository clientRepo;
+
+        public ClientsController(IClientRepository clientRepo)
+        {
+            this.clientRepo = clientRepo;
+        }
+
+        [HttpGet]
+        public IActionResult GetClients()
+        {
+            return Ok(this.clientRepo.GetAll());
+        }
+    }
+}
